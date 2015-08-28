d5d9d28704140e8edd02a29ceeb362dee2602510
Josh Davis <joshua.brent.davis@gmail.com>
2015-08-28 15:29:26 -0600

    Updated the admin menu to have an active state.

--- resources/views/includes/admin_menu.blade.php
+++ resources/views/includes/admin_menu.blade.php
@@ -1,6 +1,6 @@
 <div id="adminnav">
-	<a class="adminbtn" href="/companionships">Companionships</a>
-	<a class="adminbtn pushadmin" href="/members">Members</a>
-	<a class="adminbtn pushadmin" href="/districts">Districts</a>
-	<a class="adminbtn pushadmin" href="/stats">Stats</a>
+	<a class="adminbtn {{ \App\Http\Helpers\ViewHelper::setActive('companionships', 'filledin') }}" href="/companionships">Companionships</a>
+	<a class="adminbtn pushadmin {{ \App\Http\Helpers\ViewHelper::setActive('members', 'filledin') }}" href="/members">Members</a>
+	<a class="adminbtn pushadmin {{ \App\Http\Helpers\ViewHelper::setActive('districts', 'filledin') }}" href="/districts">Districts</a>
+	<a class="adminbtn pushadmin {{ \App\Http\Helpers\ViewHelper::setActive('stats', 'filledin') }}" href="/stats">Stats</a>
 </div>
