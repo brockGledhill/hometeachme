7e5ce8de708754d87c0eab1d6485addfb0f44b60
Josh Davis <joshua.brent.davis@gmail.com>
2016-01-13 19:45:52 -0700

    Allow all /hooks/* to bypass csrf protection. Run the git pull in the git repo when receiving a web hook.

--- app/Http/Controllers/Hooks/PullRequestController.php
+++ app/Http/Controllers/Hooks/PullRequestController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Hooks;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\App;
 
 /**
  * Class PullRequestController
@@ -17,8 +19,11 @@ class PullRequestController extends Controller {
 	 *
 	 * @return \Illuminate\Http\Response
 	 */
-	public function postPullRequest(Request $request) {
-		syslog(LOG_INFO, print_r($request->all(), true));
-		exit;
+	public function postPullRequest(Request $Request) {
+		$request = $Request->all();
+		if (App::environment() === 'qa' && $request['action'] === 'closed' && $request['pull_request']['merged'] === true && $request['pull_request']['base']['ref'] === 'master') {
+			exec('cd /var/www/qa.hometeachme.org/ && git pull');
+		}
+		return new Response('pull request received!', 200);
 	}
 }
