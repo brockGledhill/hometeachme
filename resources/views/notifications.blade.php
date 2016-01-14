f4839f409f107a5e739a5cb99b777d6bed909925
Josh Davis <joshua.brent.davis@gmail.com>
2016-01-13 20:00:00 -0700

    Change test back.

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="subcenterbox">
 
-	<h4 class="pagetitles">1 Notifications</h4>
+	<h4 class="pagetitles">0 Notifications</h4>
 
 	<p class="exptext">You have 0 notifications at this time</p>
     
